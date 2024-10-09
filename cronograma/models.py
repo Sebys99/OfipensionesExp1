72e983d36681240af2e6046b9b39ba46e09ea700
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-10-09 00:05:44 -0500

    Funcional Completo sin migraciones

--- cronograma/models.py
+++ cronograma/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Cronograma(models.Model):
-    num_cuentas = models.IntegerField(default =None)
-    monto_total = models.FloatField(default=None)
-    periodo_total = models.IntegerField(default =None)
+    num_cuentas = models.IntegerField(default =None,null=True,blank=True)
+    monto_total = models.FloatField(default=None,null=True,blank=True)
+    periodo_total = models.IntegerField(default =None,null=True,blank=True)
 
     def __str__(self):
         return '%s %s %s' % (self.num_cuentas, self.monto_total, self.periodo_total)
